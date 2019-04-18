1313e9eb6f07da67745e54a30ea9014a52478f30
Pawel Lisewski <pawel.lisewski@gmail.com>
2019-04-18 18:49:45 +0800

    fix Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,14 +1,69 @@
 import * as React from "react";
-import { styled, Box, Card, Text, Flex } from "primithemes";
+import styled, { css } from "styled-components";
+import { Container } from "src/components/Container";
+import { space, color, weight } from "src/theme";
+import { desktop } from "src/theme/media";
+import { brevier, pica, greatPrimer } from "src/theme/typography";
 import { Logo } from "src/components/Logo";
 import { Link } from "src/components/Link";
 
-const Main = styled(Card)`
+const Main = styled.div`
+  padding: ${space(3)} 0;
   position: relative;
   overflow: hidden;
+  background: ${color("grey.800")};
+  color: ${color("white.light")};
+  border-top: 4px solid ${color("primary.main")};
 `;
 
-const MainInner = styled(Flex)``;
+const MainInner = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: ${space(4)} 0;
+  color: ${color("grey.400")};
+  ${desktop(css`
+    flex-direction: row;
+  `)};
+`;
+
+const Contact = styled.div`
+  margin-top: ${space(3)};
+  text-align: center;
+  ${desktop(css`
+    margin-top: 0;
+    margin-left: ${space(4)};
+    text-align: left;
+  `)}
+`;
+
+const Title = styled.div`
+  ${greatPrimer};
+  font-weight: ${weight("bold")};
+  margin-bottom: ${space(2)};
+  color: ${color("grey.100")};
+`;
+const ContactInfo = styled.div`
+  color: inherit;
+  ${pica};
+  margin-bottom: ${space(1)};
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
+
+const Copyright = styled.p`
+  ${brevier};
+  margin-bottom: 0;
+  padding: ${space(2)};
+  text-align: center;
+  background: ${color("black.main")};
+  color: ${color("grey.600")};
+  & span {
+    color: ${color("grey.300")};
+  }
+`;
 
 interface Props {
   logo?: any;
@@ -19,60 +74,25 @@ interface Props {
 }
 
 const Footer: React.SFC<Props> = ({ logo, title, phone, email, address }) => (
-  <Box as="footer">
-    <Main
-      bg="grey.800"
-      color="white.light"
-      py={4}
-      px={3}
-      bt={4}
-      borderColor="secondary.main"
-    >
-      <MainInner
-        justifyContent="center"
-        alignItems="center"
-        flexDirection={["column", "column", "row"]}
-        py={3}
-      >
-        <Box mr={[0, 0, 4]} mb={[2, 2, 0]}>
+  <footer>
+    <Main>
+      <Container>
+        <MainInner>
           <Link to="/">
             <Logo opacity={0.9} variant="light" width={130} />
           </Link>
-        </Box>
-        <Flex justifyContent="center" flexDirection="column">
-          <Text mb={2} color="grey.300" fontSize={3} fontWeight={5}>
-            {title}
-          </Text>
-          <Box color="grey.400">
-            <Text
-              color="inherit"
-              lineHeight={2}
-              fontSize={2}
-              textAlign={["center", "center", "left"]}
-            >
-              {phone}
-            </Text>
-            <Text
-              color="inherit"
-              lineHeight={2}
-              fontSize={2}
-              textAlign={["center", "center", "left"]}
-            >
-              {email}
-            </Text>
-          </Box>
-        </Flex>
-      </MainInner>
+          <Contact>
+            <Title>{title}</Title>
+            <ContactInfo>{phone}</ContactInfo>
+            <ContactInfo>{email}</ContactInfo>
+          </Contact>
+        </MainInner>
+      </Container>
     </Main>
-    <Flex bg="black.main" p={3} justifyContent="center">
-      <Text color="grey.600" fontSize={1}>
-        © 2018 Copyright:{" "}
-        <Text as="span" color="primary.contrast">
-          {title}
-        </Text>
-      </Text>
-    </Flex>
-  </Box>
+    <Copyright>
+      © 2018 Copyright: <span>{title}</span>
+    </Copyright>
+  </footer>
 );
 
 export { Footer };
